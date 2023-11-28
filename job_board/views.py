a3beeec78225b95f0af9d9b76be19d18109386b1
samzzii <nasstromsamuel@icloud.com>
2023-11-28 16:13:10 +0100

    added the index template, fetched data from the db in views and displayed in the template

--- job_board/views.py
+++ job_board/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
+# Måste importa models för att kunna hämta data
+from .models import JobPost
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Job Board</h1>')
+    # En variabel som håller de job där is_active är true:
+    active_postings = JobPost.objects.filter(is_active=True)
+    # Dictionary som skickar data till the template. måste innehålla key-value pairs.
+    context = {
+        # Sätter job_postings som keyn och alla active postings som the value
+        'job_postings': active_postings
+        
+    }
+    return render(request, 'job_board/index.html', context)
